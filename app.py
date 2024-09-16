81673421c44d952512a6d5f23a4eb387a6119d0f
kailash Maurya <kailashmaurya51@gmail.com>
2024-09-16 09:31:03 +0530

    Updated template/index.html and app.py

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request
 import pandas as pd
-
+import os  
 app = Flask(__name__)
 
-# Dummy data (load this from your model or a CSV)
+
 rural_areas = [
     'Manali', 'Solang', 'Naggar', 'Kasol', 'Tirthan Valley',
     'Dharamshala', 'Palampur', 'Bir', 'Kangra', 'Kullu'
 ]
 hilly_areas = [
     'Rohtang Pass', 'Triund', 'Churdhar Peak', 'Parvati Valley', 'Hampta Pass',
-    'Kheer Ganga', 'Great Himalayan National Park', 'Bhrigu Lake', 'Beas Kund', 'Malana']
+    'Kheer Ganga', 'Great Himalayan National Park', 'Bhrigu Lake', 'Beas Kund', 'Malana'
+]
 
 @app.route('/')
 def index():
@@ -21,7 +22,6 @@ def predict():
     selected_rural_area = request.form.get('rural_area')
     selected_hilly_area = request.form.get('hilly_area')
 
-   
     connectivity_possible = True 
     cost_plan = {'material': 50000, 'labour': 30000, 'maintenance': 20000}  
     
@@ -32,4 +32,5 @@ def predict():
                             cost_plan=cost_plan)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
